696cba153310ce1522ba321a75d138d6424766b1
Ramrami Mohamed <ramrami.mohamed@gmail.com>
2021-11-09 19:40:57 +0100

    Added routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+import Earthquake from './components/Earthquake/Earthquake';
+import NotFound from './components/NotFound/NotFound';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/earthquake/:earthquakeId" element={<Earthquake />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
